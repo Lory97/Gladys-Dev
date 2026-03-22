023bf4fa573b06a6a35812c03fa3eb652d0674c9
Lory97 <lori.ferrand@gmail.com>
2026-03-22 16:46:02 -0400

    refactor: Extract frontend to independent repository

--- 6. Dapp/Projet3/front/voting-app/src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import VotingDashboard from '@/components/VotingDashboard'
-
-export default function Home() {
-  return <VotingDashboard />
-}
--- 6. Dapp/Projet3/front/voting-app/src/types/VotingFunctionName.ts
+++
@@ -1,9 +0,0 @@
-export type VotingFunctionName =
-    | "addProposal"
-    | "addVoter"
-    | "endProposalsRegistering"
-    | "endVotingSession"
-    | "setVote"
-    | "startProposalsRegistering"
-    | "startVotingSession"
-    | "tallyVotes"
